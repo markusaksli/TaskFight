20d88744507ea93cbc39df80838fb28d160f6e04
Markus <markusaksli@hotmail.com>
2020-02-04 23:30:17 +0200

    added splash screen

---
+++ Assets/Scripts/ShrinkSplash.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ShrinkSplash : MonoBehaviour
+{
+    Animation anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+        StartCoroutine(Shrink());
+    }
+
+    private IEnumerator Shrink()
+    {
+        anim.Play("SplashFadeIn");
+        yield return new WaitForSeconds(3.3f);
+        anim.Play("SplashFadeOut");
+    }
+}
