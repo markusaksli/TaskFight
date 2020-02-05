6ee3f93a090f9c0e254043f5bd03089e06c39107
Markus <markusaksli@hotmail.com>
2020-02-05 16:37:32 +0200

    Integrated time, fixed splash

--- Assets/Scripts/ShrinkSplash.cs
+++ Assets/Scripts/ShrinkSplash.cs
@@ -1,20 +1,27 @@
-﻿using System.Collections;
+﻿using Doozy.Engine;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class ShrinkSplash : MonoBehaviour
 {
+    VideoPlayer video;
     Animation anim;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animation>();
+        video = GetComponent<VideoPlayer>();
         StartCoroutine(Shrink());
     }
 
     private IEnumerator Shrink()
     {
         anim.Play("SplashFadeIn");
-        yield return new WaitForSeconds(6f);
+        yield return new WaitUntil(() => video.isPrepared);
+        yield return new WaitForSeconds(3f);
         anim.Play("SplashFadeOut");
+        yield return new WaitForSeconds(1f);
+        GameEventMessage.SendEvent("SplashEnded");
     }
 }
