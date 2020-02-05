256ca2ea1323ccaea53ff8d68b83b37103350260
Markus <markusaksli@hotmail.com>
2020-02-05 16:18:04 +0200

    added circle hp bar

--- Assets/EnemyManager.cs
+++ Assets/EnemyManager.cs
@@ -1,58 +1,37 @@
 ﻿using DG.Tweening;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
-    public List<Transform> enemies = new List<Transform>();
-    Collider2D col;
+    Enemy enemy;
     public Transform startPos;
     public Transform endPos;
     public bool killed = false;
-    public float lerpTime;
-    // Start is called before the first frame update
+    bool moving = false;
+
     void Start()
     {
+        DOTween.SetTweensCapacity(500, 50);
         transform.position = endPos.position;
-        col = GetComponent<Collider2D>();
-        Transform[] enemyArray = GetComponentsInChildren<Transform>();
-        foreach (Transform t in enemyArray)
-        {
-            enemies.Add(t);
-        }
-        enemies.Remove(this.transform);
+        enemy = GetComponentInChildren<Enemy>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MoveEnemies()
     {
-        if (Input.touchCount > 0 && !killed)
-        {
-            Touch touch = Input.GetTouch(0);
-            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
-            Collider2D touchCollider = Physics2D.OverlapPoint(touchPos);
-            if (touchCollider.Equals(col))
-            {
-                StartCoroutine(MoveEnemies());
-            }
-        }
-        if (!killed)
-        {
-            StartCoroutine(MoveEnemies());
-        }
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            StartCoroutine(MoveEnemies());
-        }
+        StartCoroutine(MoveRoutine());
     }
 
-    IEnumerator MoveEnemies()
+    IEnumerator MoveRoutine()
     {
         killed = true;
-        transform.position = startPos.position;
-        transform.DOMove(endPos.position, 1f).SetEase(Ease.Linear);
         yield return new WaitForSeconds(1f);
+        enemy.ResetEnemy();
+        transform.position = startPos.position;
+        moving = true;
+        transform.DOMove(endPos.position, 1f).SetEase(Ease.InOutCubic);
+        yield return new WaitForSeconds(1f);
+        moving = false;
         killed = false;
     }
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,24 +1,53 @@
-﻿using UnityEngine;
+﻿
+using Doozy.Engine.Progress;
+using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    Progressor healthBar;
     Collider2D col;
+    EnemyManager EM;
+    public int hp = 3;
 
-    private void Start()
+    void Start()
     {
+        healthBar = GetComponent<Progressor>();
+        EM = GetComponentInParent<EnemyManager>();
         col = GetComponent<Collider2D>();
     }
+
     void Update()
     {
-        if (Input.touchCount > 0)
+        if (Input.touchCount > 0 && !EM.killed)
         {
             Touch touch = Input.GetTouch(0);
             Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
             Collider2D touchCollider = Physics2D.OverlapPoint(touchPos);
             if (touchCollider.Equals(col))
             {
-                //Destroy(this.gameObject);
+                hp -= 1;
+                healthBar.SetValue(hp);
+            }
+            if (hp == 0)
+            {
+                EM.MoveEnemies();
             }
         }
+        if (Input.GetKeyDown(KeyCode.F) && !EM.killed)
+        {
+            hp -= 1;
+            healthBar.SetValue(hp);
+            if (hp == 0)
+            {
+                EM.MoveEnemies();
+            }
+        }
+    }
+
+    public void ResetEnemy()
+    {
+        hp = 3;
+        healthBar.InstantSetProgress(0);
+        healthBar.SetValue(3);
     }
 }
